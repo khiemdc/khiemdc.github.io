7a9af76ca7d20bee8cf9a979580e3100087d5bae
khiemdc <khiemle@verizon.net>
2018-03-12 01:47:14 -0400

    khiem angular portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NavComponent } from './nav/nav.component';     // Add this
+import { ShowcaseComponent } from './showcase/showcase.component';  // Add this
+import { HomeComponent } from './home/home.component';     // Add this
+import { AboutComponent } from './about/about.component';  // Add this
+import { ContactComponent } from './contact/contact.component';  // Add this
+import { ProjectComponent } from './project/project.component';  // Add this
+import { FooterComponent } from './footer/footer.component';  // Add this
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'project', component: ProjectComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'showcase', component: ShowcaseComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { QuoteComponent } from './quote/quote.component';
+import { DownloadTemplateComponent } from './download-template/download-template.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ShowcaseComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectComponent,
+    ContactComponent,
+    QuoteComponent,
+    DownloadTemplateComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
